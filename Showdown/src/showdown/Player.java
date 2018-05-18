ce15701c3247596862ea7ca830710d8b9d9da91c
jlaur <jlaur@DESKTOP-MH0MSVI>
2018-05-18 09:49:44 -0400

    ur boi slammed some lines into Player.java

--- Showdown/src/showdown/Player.java
+++ Showdown/src/showdown/Player.java
@@ -1,5 +1,15 @@
 package showdown;
+import java.util.ArrayList;
 
 public class Player {
-
+	private String name;
+	private ArrayList<Card> deck;
+	private Card[] hand = new Card[6];
+	private creature[] field = new creature[7];
+	public Player(String name, ArrayList deck, Card[] hand, creature[] field) {
+		this.name = name;
+		this.deck = deck;
+		this.hand = hand;
+		this.field = field;
+	}
 }
