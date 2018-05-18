fb402e9855d8200d8adb177868974f686e6dff23
jlaur <jlaur@DESKTOP-MH0MSVI>
2018-05-18 09:15:55 -0400

    doit

--- Showdown/src/showdown/Game.java
+++ Showdown/src/showdown/Game.java
@@ -4,6 +4,7 @@ public class Game {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, this is a github test.");
+		System.out.println("Hello, this is a your mom test. ");
 	}
 
 }
